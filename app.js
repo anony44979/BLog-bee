63874e45dccb4ac9a05e66bdac8d9b2775a2dd07
Bhushan G Y <bhushangy44@gmail.com>
2020-03-27 18:42:40 +0530

    firebase set up

--- app.js
+++ app.js
@@ -3,3 +3,25 @@ var express = require("express"),
 	bodyParser = require("body-parser"),
     expressSanitizer = require("express-sanitizer"),
     methodOverride = require('method-override');
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.set("view engine", "ejs");
+app.use(express.static("public"));//telling it to look inside public direc where custom stylesheet
+app.use(methodOverride('_method'));
+
+
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+
+
+
+app.listen(3000,function(){
+    console.log("BLog-bee server has started")
+  
+});
+
